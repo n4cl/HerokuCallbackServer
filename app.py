f369b48875e6110660cd9af70da476411ab4932a
n4cl <devn4cl@gmail.com>
2022-04-20 19:09:25 +0900

    Add: 入力データのログ出力処理を追加

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, jsonify
+from datetime import datetime
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def logging_request(_data: dict, file_type: str):
+    print({"timestamp": datetime.now(),
+           "data": _data,
+           "file_type": file_type})
+
+@app.get('/')
 def hello():
     return jsonify({"message": "Hello"})
+
+@app.post('/')
+def post():
+    if request.content_type == "application/json":
+        logging_request(request.get_json(), "application/json")
+    if request.form:
+        logging_request(dict(request.form), "form")
+
+    return jsonify({"message": "post"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
